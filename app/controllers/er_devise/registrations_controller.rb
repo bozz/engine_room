f69eae02bc4e90d047b1930516d5428d4c2f7acf
Boris Searles <boris@lucidgardens.com>
2010-11-17 13:00:13 +0100

    added editing page for user

--- app/controllers/er_devise/registrations_controller.rb
+++ app/controllers/er_devise/registrations_controller.rb
@@ -1,5 +1,7 @@
 module ErDevise
   class RegistrationsController < Devise::RegistrationsController
+    before_filter :authenticate_er_devise_user!
+    layout 'engine_room'
     prepend_view_path(__FILE__ + "../../views")
   end
 end
