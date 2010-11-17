78103572622136987a7843ea107b82d5358e57c1
Boris Searles <boris@lucidgardens.com>
2010-11-17 11:20:58 +0100

    updated styling of login/forgot_password pages

--- app/controllers/er_devise/passwords_controller.rb
+++ app/controllers/er_devise/passwords_controller.rb
@@ -1,5 +1,6 @@
 module ErDevise
   class PasswordsController < Devise::PasswordsController
+    layout 'login'
     prepend_view_path(__FILE__ + "../../views")
   end
 end
--- app/controllers/er_devise/sessions_controller.rb
+++ app/controllers/er_devise/sessions_controller.rb
@@ -1,5 +1,6 @@
 module ErDevise
   class SessionsController < Devise::SessionsController
+    layout 'login'
     prepend_view_path(__FILE__ + "../../views")
   end
 end
