ad5735b50a1b199fef88c000849a8126bb8f3abc
Boris Searles <boris@lucidgardens.com>
2010-11-17 11:27:12 +0100

    javascript cleanup

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,15 +3,6 @@
 */
 
 $(document).ready(function() {
-	
-	SyntaxHighlighter.all();
-	
-	$("aside").mouseover(function(){
-		$(this).addClass('open');
-	});
-	$("aside").mouseout(function(){
-		$(this).removeClass('open');
-	});
 
 });
 
