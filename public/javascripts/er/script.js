110035468cabafa243480b26944072df28ef95db
Boris Searles <boris@lucidgardens.com>
2010-12-06 21:31:13 +0100

    added jquery ui lib; setup of tabs for has_many relationships and section editing

--- public/javascripts/er/script.js
+++ public/javascripts/er/script.js
@@ -4,6 +4,17 @@
 
 $(document).ready(function() {
 
+  $("#tabs").tabs();
+
+  $("#overview-fields-table tbody").sortable({
+    axis: 'y',
+    opacity: 0.6,
+    stop: function(event, ui){
+      ui.item.effect("highlight", {}, 500);
+    }
+  });
+  //$("#overview-fields-table tbody").disableSelection();
+
   // automatically reload forms when select has changed
   $("form div.reload select").change(function(){
     var hiddenField = document.createElement('input');
